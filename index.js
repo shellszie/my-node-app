ea8798d64fc75012a152637bf9d1d88f11d0b63f
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2024-11-23 14:42:09 -0800

    formatting code

--- index.js
+++ index.js
@@ -13,43 +13,39 @@ app.use(cors());  // Allow all domains, or specify the origin if you want to lim
 
 // Define a route for the root URL
 app.get('/api', (req, res) => {
-  
-  console.log("params for name=" + req.query.name);
-  console.log("params for animal=" + req.query.animal);
-  res.json({message: 'Hello from the API!'});
+    console.log("params for name=" + req.query.name);
+    console.log("params for animal=" + req.query.animal);
+    res.json({message: 'Hello from the API!'});
 });
 
 app.get('/googlebooks', async (req, res) => {
-  
-  let search = req.query.searchTerm;
-  try {
-    await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${api_key}`)
-      .then(response => {
-        return res.send(response.data);      
-      })
-      } catch (error) {
+    let search = req.query.searchTerm;
+    try {
+        await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${api_key}`)
+            .then(response => {
+                return res.send(response.data);
+            })
+    } catch (error) {
         console.error(error.message);
-      } 
+    }
 });
 
 app.get('/previewExists', async (req, res) => {
     let isbn = req.query.isbn;
-  try {
-    await axios.get(`https://books.google.com/books?jscmd=viewapi&bibkeys=ISBN:${isbn}`)
-      .then(response => {
-        if (response.data.includes("noview")) {
-            return res.send(false);
-        }
-        else {
-            return res.send(true);
-        }
-      })
-      } catch (error) {
+    try {
+        await axios.get(`https://books.google.com/books?jscmd=viewapi&bibkeys=ISBN:${isbn}`)
+            .then(response => {
+                if (response.data.includes("noview")) {
+                    return res.send(false);
+                } else {
+                    return res.send(true);
+                }
+            })
+    } catch (error) {
         console.error(error.message);
-      }
+    }
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
