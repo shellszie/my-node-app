1c11f7ea12be97eb79082e6c83c0809adbecc042
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2024-11-26 08:43:29 -0800

    printing out api key for debugging

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.get('/api', (req, res) => {
 app.get('/googlebooks', async (req, res) => {
     let search = req.query.searchTerm;
     try {
+        console.log("api key = " + api_key);
         let response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${api_key}&maxResults=40`);
         res.send(response.data);
     }
